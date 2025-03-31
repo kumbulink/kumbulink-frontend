6216e3fdf82e440b0eb55ea99f08bd8b4b1dc389
Willian Viana <willian.batista.viana@gmail.com>
2025-03-31 18:14:25 -0300

    hore(actions): testing actions

---
+++ src/Pages/Home/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import { SearchIcon, FilterIcon } from '@/shared/ui/icons'
+
+export const SearchBar = () => (
+  <div className='relative'>
+    <input
+      type='text'
+      placeholder='Digite o valor, moeda ou localidade'
+      className='w-full bg-white rounded-lg py-3 px-10 shadow-sm'
+    />
+    <SearchIcon className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400' />
+    <FilterIcon className='absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400' />
+  </div>
+)
---
+++ src/Pages/offer/index.ts
@@ -0,0 +1,1 @@
+export { ExchangeOfferPage } from './ExchangeOfferPage'
