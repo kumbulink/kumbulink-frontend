64f93882ef10ae8e80c3ed402b51bda0f90755fc
Willian Viana <willian.batista.viana@gmail.com>
2025-04-01 11:13:52 -0300

    chore(home): add blur to the bottom of the page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -100,12 +100,8 @@ export const HomePage = () => {
         <SearchBar />
       </div>
 
-      <main className='bg-gray-100 min-h-[calc(100vh-8rem)]'>
-        <div className='px-4 pt-4'>
-          <div className='flex justify-end items-center'>
-            <span className='text-gray-700'>Filtro (1)</span>
-          </div>
-
+      <main className='bg-gray-100 min-h-[calc(100vh-8rem)] relative'>
+        <div className='px-4 pt-4 pb-32'>
           {offers.length === 0 && (
             <div className='space-y-4 mt-4 pl-5 pr-5'>
               <p className='text-gray-500 text-2xl text-center mt-48'>
@@ -115,7 +111,7 @@ export const HomePage = () => {
           )}
 
           {offers.length > 0 && (
-            <div className='space-y-4 mt-4 pb-24'>
+            <div className='space-y-4 mt-4'>
               {offers.map((offer, index) => (
                 <OfferCard key={index} {...offer} />
               ))}
@@ -123,7 +119,17 @@ export const HomePage = () => {
           )}
         </div>
 
-        <div className='fixed bottom-8 left-4 right-4'>
+        <div
+          className='fixed bottom-0 left-0 right-0 h-48 pointer-events-none'
+          style={{
+            background:
+              'linear-gradient(to top, rgb(243 244 246) 15%, rgba(243, 244, 246, 0.9) 30%, rgba(243, 244, 246, 0.7) 50%, rgba(243, 244, 246, 0.3) 70%, rgba(243, 244, 246, 0) 85%)',
+            backdropFilter: 'blur(1.5px)',
+            WebkitBackdropFilter: 'blur(1.5px)'
+          }}
+        />
+
+        <div className='fixed bottom-8 left-4 right-4 z-50'>
           <button
             onClick={handleAnunciarClick}
             className='w-full bg-primary-orange text-white py-4 font-medium rounded-lg'
