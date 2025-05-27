23da73c31c8fcc6b298b51cfa2f0b4e8b35c2398
Willian Viana <willian.batista.viana@gmail.com>
2025-05-27 11:41:10 -0300

    chore(country): remove Portugal from the list

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -13,7 +13,7 @@ export const Popup = ({ isOpen, onClose, children }: PopupProps) => {
   return (
     <div className='fixed inset-0 z-50 flex items-center justify-center p-4'>
       <div className='fixed inset-0 bg-black opacity-50' onClick={onClose} />
-      <div className='relative overflow-y-auto h-auto max-h-[90vh] rounded-lg'>
+      <div className='relative overflow-y-auto h-auto max-h-[90vh]'>
         <button
           onClick={onClose}
           className='absolute right-5 top-5 p-1 cursor-pointer h-5 w-5 z-9'
