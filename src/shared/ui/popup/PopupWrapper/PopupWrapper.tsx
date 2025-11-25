f358ee3298effed8210f7ceb677e47b7201fc400
Willian Viana <willian.batista.viana@gmail.com>
2025-11-25 14:51:22 -0300

    fix(dropdown): remove overflow-y-hidden

--- src/shared/ui/popup/PopupWrapper/PopupWrapper.tsx
+++ src/shared/ui/popup/PopupWrapper/PopupWrapper.tsx
@@ -19,7 +19,7 @@ export const PopupWrapper = ({
     <div className='fixed inset-0 z-50 flex items-center justify-center p-4'>
       <div className='fixed inset-0 bg-black opacity-50' onClick={onClose} />
       <div
-        className={`relative overflow-y-auto h-auto max-h-[90vh] overflow-y-hidden`}
+        className={`relative h-auto max-h-[90vh]`}
       >
         <button
           onClick={onClose}
